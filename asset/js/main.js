29fa5ae5de9b7c142f9cc6f7d6ea6ba9af9be9f8
hoangfg <121874187+hoangfg@users.noreply.github.com>
2023-05-02 10:13:00 +0700

    admin-template

---
+++ asset/js/main.js
@@ -0,0 +1,18 @@
+/* =================LEFT SIDEBAR START STYLE===================== */
+$(document).on('click', '#sidebar li', function () {
+    $(this).addClass('active').siblings().removeClass('active')
+});
+/* =================LEFT SIDEBAR DP TOGGLE START STYLE===================== */
+$('.sub-menu ul').hide();
+$('.sub-menu a').click(function () {
+    $(this).parent('.sub-menu').children('ul').slideToggle('100')
+    $(this).find('.right').toggleClass('fa-caret-up fa-caret-down')
+});
+
+/* ==================SIDEBAR TOGGLE ===================== */
+$(document).ready(function () {
+    $('#toggleSidebar').click(function () {
+        $(".left-menu").toggleClass("hide");
+        $(".content-wrapper").toggleClass("hide");
+    });
+});
